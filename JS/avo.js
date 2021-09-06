17704fdb62b3e81f8466ef7579379f596ab49bab
Leticia Bedoni <leticiabedoni@hotmail.com>
2021-09-06 12:07:49 -0300

    Adicionando JS a página da avo e botão de escolha

---
+++ JS/avo.js
@@ -0,0 +1,11 @@
+function escolha(){
+    var escolha=prompt("Qual sua escolha?");
+
+    if(escolha == 1){
+    alert("VOCÊ VENCEU! Agora que Moana sabe dos segredos da Ilha que você contou, ela poderá desbravar os Oceanos para salva-la! 🌊");
+}
+
+    if(escolha==2){
+    alert("GAME OVER! O pai de Moana ficou enfurecido e pediu para que não falasse nesse assunto novamente. ☹")
+}
+}
