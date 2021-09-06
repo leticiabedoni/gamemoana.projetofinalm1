96fd4f52cd5a5865030662dcbd5d48f027c0258d
Leticia Bedoni <leticiabedoni@hotmail.com>
2021-09-06 13:33:13 -0300

    Estilizando e criando novas páginas para as escolhas da personagem Moana

--- JS/moana.js
+++ JS/moana.js
@@ -2,10 +2,13 @@ function escolha(){
     var escolha=prompt("Qual sua escolha?");
 
     if(escolha == 1){
-    alert("GAME OVER! Seu pai ficou furioso e pediu para que não falasse nunca nesse assunto!");
+        window.location="../HTML/game-over-moana.html";
+
+}
+    if(escolha==2){
+        window.location="../HTML/moana-win.html";
+}
 }
 
-    if(escolha==2){
-    alert("VOCÊ VENCEU! A sua avó te contou os mais profundos segredos da Ilha e agora você poderá salva-la!🎉")
-}
-}
+//alert("GAME OVER! Seu pai ficou furioso e pediu para que não falasse nunca nesse assunto!");
+//alert("VOCÊ VENCEU! A sua avó te contou os mais profundos segredos da Ilha e agora você poderá salva-la!🎉")
