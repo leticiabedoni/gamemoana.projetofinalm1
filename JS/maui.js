19f90da9da24327fb8ec0d485819f1e76702e1ab
Leticia Bedoni <leticiabedoni@hotmail.com>
2021-09-06 12:17:03 -0300

    Adicionando JS e botão a página do Maui

---
+++ JS/maui.js
@@ -0,0 +1,11 @@
+function escolha(){
+    var escolha=prompt("Qual sua escolha?");
+
+    if(escolha == 1){
+    alert("VOCÊ VENCEU! Você tomou a melhor atitude possível e graças a sua parceria com Moana a Ilha será salva! 🌊");
+}
+
+    if(escolha==2){
+    alert("GAME OVER! Sem a sua ajuda Moana não encontrará o coração de Te Fiti a tempo. ☹")
+}
+}
